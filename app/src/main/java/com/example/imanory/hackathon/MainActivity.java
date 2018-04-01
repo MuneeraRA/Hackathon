b61942479cd0f54aaa3fa7a849bc608d69452279
imanory <mun.rashed@gmail.com>
2018-04-01 13:55:37 +0300

    Adding shapes for UI Design

--- app/src/main/java/com/example/imanory/hackathon/MainActivity.java
+++ app/src/main/java/com/example/imanory/hackathon/MainActivity.java
@@ -12,7 +12,5 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toast.makeText(this,"",Toast.LENGTH_LONG).show();
         Toast.makeText(this, "Muneerah Toast Message", Toast.LENGTH_SHORT).show();
-        Toast.makeText(this,"jana",Toast.LENGTH_LONG).show();
-
-    }
+        Toast.makeText(this,"jana",Toast.LENGTH_LONG).show();}
 }
